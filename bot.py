d50e4b93bad99ef0b32b10644f2d669125d4bf87
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-06 11:20:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
 import requests
-import discord
+import pyrogram
+from pyrogram import Client, filters
+from config import BOT_TOKEN, API_HASH, API_ID
 
-client = discord.Client(intents=discord.Intents.all())
+bot = Client(name='DepremBot', bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-
-TOKEN = "TOKEN"
-
-CHANNEL_ID = "KANAL ID"
 
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-12-31&minmagnitude=4&limit=1&orderby=time&latitude=39&longitude=35&maxradiuskm=1000"
 
@@ -16,12 +14,13 @@ def get_earthquake_info():
     earthquake_info = data['features'][0]['properties']
     return earthquake_info
 
-@client.event
-async def on_ready():
+@Client.on_message(filters.command('deprem')
+async def deprem(bot, message)
     print(f"Bot is ready, running on {client.user}")
-    channel = client.get_channel(int(CHANNEL_ID))
     earthquake_info = get_earthquake_info()
     message = f"**DİKKAT! TÜRKİYEDE DEPREM!!!:**\nBüyüklük: {earthquake_info['mag']}\nLokasyon: {earthquake_info['place']}\nZaman: {earthquake_info['time']}\nDetaylı Bilgi: {earthquake_info['url']}"
-    await channel.send(message)
+    await bot.send_message(
+        chat_id=message.chat.id, 
+        text=message) 
 
-client.run(TOKEN)
+bot.run()
