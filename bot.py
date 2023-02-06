83993e7fd949f84625da9790c5fca3b604766f42
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-06 12:02:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ from pyrogram import Client, filters
 from config import BOT_TOKEN, API_HASH, API_ID
 import json
 
-bot = Client("DepremBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+Bot = Client("DepremBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-12-31&minmagnitude=4&limit=1&orderby=time&latitude=39&longitude=35&maxradiuskm=1000"
 
-@Client.on_message(filters.command("deprem"))
+@Bot.on_message(filters.command("deprem"))
 async def deprembilgi(bot, message):
     try:
         text = "**DİKKAT! TÜRKİYEDE DEPREM!!!:**\nBüyüklük: {earthquake_info['mag']}\nLokasyon: {earthquake_info['place']}\nZaman: {earthquake_info['time']}\nDetaylı Bilgi: {earthquake_info['url']}"
@@ -22,4 +22,4 @@ async def deprembilgi(bot, message):
             chat_id=message.chat.id,
             text=f"`{e}`")
 
-bot.run()
+Bot.run()
