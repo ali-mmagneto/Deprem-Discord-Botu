2e66803632cc801fc53d6b644c777a6641d0727e
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-06 11:28:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = Client(name='DepremBot', bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_
 
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-12-31&minmagnitude=4&limit=1&orderby=time&latitude=39&longitude=35&maxradiuskm=1000"
 
-def get_earthquake_info():
+def get_earthquake_info(bot, message):
     response = requests.get(url)
     data = response.json()
     earthquake_info = data['features'][0]['properties']
@@ -17,7 +17,7 @@ def get_earthquake_info():
 @Client.on_message(filters.command('deprem'))
 async def deprem(bot, message):
     print(f"Bot is ready, running on {client.user}")
-    earthquake_info = get_earthquake_info()
+    earthquake_info = get_earthquake_info(bot, message)
     message = f"**DİKKAT! TÜRKİYEDE DEPREM!!!:**\nBüyüklük: {earthquake_info['mag']}\nLokasyon: {earthquake_info['place']}\nZaman: {earthquake_info['time']}\nDetaylı Bilgi: {earthquake_info['url']}"
     await bot.send_message(
         chat_id=message.chat.id, 
