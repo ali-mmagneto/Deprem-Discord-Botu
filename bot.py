992131bf9d28578a774fd6d234661e484a3432d1
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-06 12:06:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,10 @@ url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime
 @Bot.on_message(filters.command("deprem"))
 async def deprembilgi(bot, message):
     try:
-        text = "**DİKKAT! TÜRKİYEDE DEPREM!!!:**\nBüyüklük: {earthquake_info['mag']}\nLokasyon: {earthquake_info['place']}\nZaman: {earthquake_info['time']}\nDetaylı Bilgi: {earthquake_info['url']}"
+        response = requests.get(url)
+        data = json.loads(response.text)
+        earthquake_info = data['features'][0]['properties']
+        text = f"**DİKKAT! TÜRKİYEDE DEPREM!!!:**\nBüyüklük: {earthquake_info['mag']}\nLokasyon: {earthquake_info['place']}\nZaman: {earthquake_info['time']}\nDetaylı Bilgi: {earthquake_info['url']}"
         await bot.send_message(
             chat_id=message.chat.id, 
             text=text) 
