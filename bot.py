e1e353bac293598dd1b1ef838234375a3c267d93
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-07 11:47:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def deprembilgi(bot, message):
         response = requests.get(url)
         data = response.json()
         bilgi = data['data'][0]
-        text = f"**TÜRKİYE'DE YAŞANAN SON DEPREM!!!:**\nBüyüklük: {bilgi['ml']}\nDerinlik: {bilgi['derinlik_km']}\nLokasyon: [{bilgi['yer']}]({dadresurl}\nTarih: {bilgi['tarih']}\nSaat: {bilgi['saat']}"
+        text = f"**TÜRKİYE'DE YAŞANAN SON DEPREM!!!:**\nBüyüklük: {bilgi['ml']}\nDerinlik: {bilgi['derinlik_km']}\nLokasyon: [{bilgi['yer']}]({dadresurl})\nTarih: {bilgi['tarih']}\nSaat: {bilgi['saat']}"
         latitude1 = f"{bilgi['enlem_n']}"
         longitude1 = f"{bilgi['boylam_e']}"
         dadresurl = 'https://maps.google.com/maps?q=' + latitude1 + ',' + longitude1
