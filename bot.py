128ab198f70a5897e7f9c95eb2916e100c1f27e8
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-07 13:48:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def eczanebilgi(bot, message):
             chat_id=message.chat.id,
             text="`Hatalı Kullanım`")
     else:
-        yer = unidecode(message.text).upper().split()
+        yer = unidecode(message.text).lower().split()
         il = yer[1]
         ilce = yer[2]
         eczane_url = f"https://www.nosyapi.com/apiv2/pharmacyLink?city={il}&county={ilce}&apikey=aYG3s2ErzrWUUl7Xt6RrTzve0zm3rb5gfgYHfoh9IBTO84ZhFp7dgi6wz7C6"
