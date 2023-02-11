2a8d5a763629271b9c4fc6d317e220073d185918
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-11 22:53:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,13 +111,13 @@ async def deprembilgi(bot, message):
         response = requests.get(url)
         data = response.json()
         bilgi = data['data']
-        text = "**TÜRKİYE'DE YAŞANAN SON DEPREMLER!!!:**"
+        text = "**TÜRKİYE'DE YAŞANAN SON DEPREMLER!!!\n\n:**"
         for i in bilgi:
             say += 1
             latitude1 = f"{i['enlem_n']}"
             longitude1 = f"{i['boylam_e']}"
             dadresurl = 'https://maps.google.com/maps?q=' + latitude1 + ',' + longitude1
-            text += f"\nBüyüklük: {i['ml']}\nDerinlik: {i['derinlik_km']}\nLokasyon: [{i['yer']}]({dadresurl})\nTarih: {i['tarih']} {i['saat']}\n\n"
+            text += f"Büyüklük: {i['ml']}\nDerinlik: {i['derinlik_km']}\nLokasyon: [{i['yer']}]({dadresurl})\nTarih: {i['tarih']} {i['saat']}\n\n"
             if say == int(sayi):
                 await bot.send_message(
                     chat_id=message.chat.id, 
