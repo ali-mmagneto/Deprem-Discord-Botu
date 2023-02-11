5a582e1f7116790d7fd687a02ee2d3363d70632e
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-11 19:39:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,9 +34,8 @@ async def eczanebilgi(bot, message):
         il = yer[1]
         ilce = yer[2]
         istek = NobetciEczane(il, ilce)
-        veri = istek.gorsel()
         text = "Nöbetçi Eczaneler:"
-        for i in veri['veri']:
+        for i in json.loads(istek.gorsel())["veri"]:
             text += f"İsim: {i['ad']}\nAdres: {i['adres']}\nTarif: {i['tarif']}\n Telefon No: {i['telefon']}\n\n"
         await bot.send_message(
             chat_id=message.chat.id,
