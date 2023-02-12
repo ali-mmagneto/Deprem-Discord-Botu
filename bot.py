efb19d421bb9f36e3f8e6a48d2544c381dd9581c
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-12 16:17:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,32 @@ telegraph.create_account(short_name='deprembot')
 
 url = "https://hasanadiguzel.com.tr/api/sondepremler"
 
+from pyrogram import Client, filters
+from KekikSpatula import HavaDurumu
+from unidecode import unidecode
+import json
+import re
+
+@Bot.on_message(filters.command('hava'))
+async def havaa(bot, message):
+    try:
+        ev = unidecode(message.text).split()
+        if len(ev) < 3:
+            await bot.send_message(message.chat.id, "Hatalı Kullanım :/ Doğru Kullanım Şu Şekilde:\n\n`/hava İstanbul Avcılar`") 
+            return
+        il = ev[1]
+        ilce = ev[2]
+        istek = HavaDurumu(il, ilce)
+        text = ""
+        for i in json.loads(istek.gorsel())["veri"]:
+            text += f"{i['yer']} İçin:\nHava Durumu: `{i['derece']}`\nVakit: `{i['gun']}`"
+        await bot.send_message(
+           chat_id=message.chat.id,
+           text=text)
+    except Exception as e:
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text=f"`{e}`")
 
 @Bot.on_message(filters.command("start"))
 async def start(bot, message):
